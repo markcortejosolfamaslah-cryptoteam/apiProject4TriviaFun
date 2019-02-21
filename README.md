1a0ca7e1ae6ac0dc4121b0329304d1c15e245cfb
Olfa <maslah.olfa@gmail.com>
2019-02-21 16:35:48 -0500

    change API

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # apiProject4TriviaFun
 We will create a trivia game!
+
+1/ when the page is loaded, instructions appear with a button start
+2/we use Api to provide questions
+3/when user press start button he gets a question randomly 
+4/2options true or false (button answer)
+5/on press button answer we get userInput
+6/compare userInput to the answer and provide feedback to user with changing the background color of the button, RED: Wrong answer / GREEN: Correct answer
+7/transition to next question same process for each question
+8/ after 40 questions, stop the game
+
+Stretch goal
+add timer
+add score 
+The user can pause the game but he will loose point each 5 sec paused
+firebase
--- scripts/script.js
+++ scripts/script.js
@@ -3,34 +3,39 @@ const myApp = {};
 myApp.init = function () {
 
 	myApp.setup();
-};
 
+};
+myApp.Url = ['https://opentdb.com/api.php?amount=20&category=18', 'https://opentdb.com/api.php?amount=22&category=19']
 
 
 myApp.setup = function () {
-	myApp.myEncryptWords = [
-		{ "hackeryou": "130C0E16101D241A20" },
-		{ "18H00": "030A130202" },
-		{ "red hat": "1D100F48130C1F" }
-	]
+
 }
 $(function () {
 	myApp.init();
 })
 
-myApp.requestApi = (userInput) => {
-	$.ajax({
-		url: 'http://api.trytodecrypt.com/encrypt',
-		method: 'GET',
-		dataType: 'json',
-		data: {
-			key: 'f5d3c98db90eee259b9c41beaa4ecdcd',
-			id: 1,
-			text: userInput
-		}
 
-	})
-}
-myApp.fetUserInput = () => {
+
+/* myApp.requestApi = () => {
+	for (i = 0; i < myApp.Url.length; i++) {
+		$.ajax({
+			url: myApp.Url[i],
+			method: 'GET',
+			dataType: 'json',
+
+
+		}).then(function (results) {
+			console.log(results)
+		})
+
+	}
+
+} */
+
+
+
+
+myApp.getUserInput = () => {
 
 }
