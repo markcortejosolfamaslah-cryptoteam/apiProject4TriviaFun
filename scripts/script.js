9550e3278d86ed205ca737d6ac96caca45814596
MarkCortejos <markcortejos@gmail.com>
2019-02-21 19:58:50 -0500

    finished shuffleArray

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,25 @@ $(function () {
 myApp.getUserInput = () => {
 
 }
+
+
+
+// solution from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+	// Array Shuffle Function (MUTATES PASSED ARRAY)
+		// shuffles the contents of an array
+		// returns the mutated array
+myApp.shuffleArray = function (array) {
+	// for loop to iterate through array
+	for (let i = 0; i <= array.length; i++) {
+		// create random number and store in variable
+		const randNum = Math.floor(Math.random() * (array.length + 1));
+		// create a temporary variable to store current array element
+		const temp = array[i];
+		// overwrite current array element with randomized array element
+		array[i] = array[randNum];
+		// use temp variable to add back the old current array into the randomized array element
+		array[randNum] = temp;
+	}
+	// return mutated array
+	return array
+}
